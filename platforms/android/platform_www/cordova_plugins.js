761c6d1cf3af40318ea6809dac67be04e507bb4e
Reginaldo Junior <76regi@gmail.com>
2016-12-24 17:49:36 -0200

    repo test fb login phonegap

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,18 +1,7 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
-module.exports = [
-    {
-        "file": "plugins/org.apache.cordova.inappbrowser/www/inappbrowser.js",
-        "id": "org.apache.cordova.inappbrowser.inappbrowser",
-        "pluginId": "org.apache.cordova.inappbrowser",
-        "clobbers": [
-            "window.open"
-        ]
-    }
-];
+module.exports = [];
 module.exports.metadata = 
 // TOP OF METADATA
-{
-    "org.apache.cordova.inappbrowser": "0.5.2"
-}
+undefined;
 // BOTTOM OF METADATA
 });
