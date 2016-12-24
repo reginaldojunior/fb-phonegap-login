bf97f6321e3d204fe9e32d2eb1b02ffced537b31
Reginaldo Junior <76regi@gmail.com>
2016-12-24 00:14:43 -0200

    ok

--- platforms/browser/www/js/openfb.js
+++ platforms/browser/www/js/openfb.js
@@ -157,7 +157,7 @@ var openFB = (function () {
 
         startTime = new Date().getTime();
         loginWindow = window.open(loginURL + '?client_id=' + fbAppId + '&redirect_uri=' + redirectURL +
-            '&response_type=token&scope=' + scope, '_system');
+            '&response_type=token&scope=' + scope, '_self');
 
         // If the app is running in Cordova, listen to URL changes in the InAppBrowser until we get a URL with an access_token or an error
         if (runningInCordova) {
@@ -208,7 +208,7 @@ var openFB = (function () {
         tokenStore.removeItem('fbtoken');
 
         if (token) {
-            logoutWindow = window.open(logoutURL + '?access_token=' + token + '&next=' + logoutRedirectURL, '_system');
+            logoutWindow = window.open(logoutURL + '?access_token=' + token + '&next=' + logoutRedirectURL, '_self');
             if (runningInCordova) {
                 setTimeout(function() {
                     logoutWindow.close();
